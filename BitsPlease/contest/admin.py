3bb8fed9152b06d24b3bd6bc9dbd20cc973f49f2
Amit Bansal <007amitbansal@gmail.com>
2017-09-15 21:13:05 +0530

    admin panel

--- BitsPlease/contest/admin.py
+++ BitsPlease/contest/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import Question, Contest, Tag, Notice
 
-admin.site.register(Question)
-admin.site.register(Notice)
-admin.site.register(Contest)
-admin.site.register(Tag)
+
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'publish', 'date')
+
+
+class ContestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publish', 'division', 'contest_date')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publish', 'tag', 'add_date', 'contest', 'answer')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Notice, NoticeAdmin)
+admin.site.register(Contest, ContestAdmin)
+admin.site.register(Tag, TagAdmin)
